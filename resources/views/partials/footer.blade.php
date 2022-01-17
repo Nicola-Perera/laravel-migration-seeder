944942813d4cf2fde0efbc43f0289d8e00a000eb
Nicola-Perera <nicola.perera96@mail.com>
2022-01-17 01:28:08 +0100

    add general app layout and home page content

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,4 @@
+<footer>
+    <div class="copyrights"></div>
+    <div class="contacts"></div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,9 @@
+<header>
+    <div class="top_header">
+        <div class="logo"></div>
+        <div class="contacts"></div>
+    </div>
+    <div class="bottom_header">
+        <div class="menu"></div>
+    </div>
+</header>
